7b3b260dcf87a7364f74dbaf16c9403b62787993
Gabriel Santos Cardoso <eng.gabrielscardoso@gmail.com>
2024-03-14 17:24:29 -0300

    update relation form

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -21,7 +21,7 @@ class ProductResource extends Resource
         return $form
             ->schema([
                 Forms\Components\Select::make('category_id')
-                    ->relationship('category', 'title')
+                    ->relationship('category', 'id')
                     ->required(),
                 Forms\Components\TextInput::make('name')
                     ->required()
